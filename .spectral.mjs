932be9f9face5ccf482585573fe30de562f52444
qa-rncyx-0-gscd@example.com <qa-rncyx-0-gscd@example.com>
2022-12-21 08:47:26 -0700

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-14708.stoplight-dev.com/api/v1/projects/cHJqOjMwMQ/spectral.js?branch=master&token=6ac6f6f7-88d2-4661-93c5-8ecd5099c86d";
+export default { extends: ruleset };
